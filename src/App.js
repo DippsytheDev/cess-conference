f71275c7c32042f6e4e8dc6d1ba8699134a1e0af
Akinde Dipo <akindedipo@gmail.com>
2025-07-17 22:54:23 +0100

    First

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Speakers from './components/Speakers';
-import Judges from './components/Judges';
-import Finalists from './components/Finalists';
-import Team from './components/Team';
-import EventGallery from './components/EventGallery';
-import Schedule from './components/Schedule';
-import Registration from './components/Registration';
-import Footer from './components/Footer';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Speakers from "./components/Speakers";
+import Judges from "./components/Judges";
+import Finalists from "./components/Finalists";
+import Team from "./components/Team";
+import EventGallery from "./components/EventGallery";
+import Schedule from "./components/Schedule";
+import Registration from "./components/Registration";
+import Footer from "./components/Footer";
+import { StagewiseToolbar } from "@stagewise/toolbar-react";
+import ReactPlugin from "@stagewise-plugins/react";
 
 function App() {
   return (
     <div className="App">
+      <StagewiseToolbar config={{ plugins: [ReactPlugin] }} />
       <Header />
       <Hero />
       <About />
